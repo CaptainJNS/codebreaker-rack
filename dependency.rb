317f2d39b607494df07a820433a15e1118ae4c24
Jason <sereda9623@gmail.com>
2019-08-08 12:28:51 +0300

    I18n

--- dependency.rb
+++ dependency.rb
@@ -1,4 +1,6 @@
 require 'codebreaker_captainjns'
+require 'i18n'
+require_relative './entities/config/i18n'
 require 'rack'
 require_relative 'entities/table_row'
 require_relative 'entities/data_utils'
